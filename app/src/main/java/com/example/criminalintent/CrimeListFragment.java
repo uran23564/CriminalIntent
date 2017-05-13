6c8c83bc04432f73ad83b5c3c9ec5a0aed2ddb4d
uran23564 <uran23564@gmail.com>
2017-05-13 15:20:59 +0200

    asdf

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.java
@@ -1,11 +1,27 @@
 package com.example.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by merz_konstantin on 5/13/17.
  */
 
 public class CrimeListFragment extends Fragment {
-    // TODO
+    private RecyclerView mCrimeRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view=inflater.inflate(R.layout.fragment_crime_list,container);
+
+        mCrimeRecyclerView=(RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity())); // RecyclerView braucht einen LayoutManager,
+        // der die Liste auf dem Screen erzeugt und sie managed.
+        // Wir erzeugen hier einen LinearLayoutManager und uebergeben diesen an den RecyclerView.
+    }
 }
