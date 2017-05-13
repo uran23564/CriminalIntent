a931bc2b43c4c11e7f107c99d77a196b11f90050
uran23564 <uran23564@gmail.com>
2017-05-13 11:28:52 +0200

    asdf

---
+++ app/src/main/java/com/example/criminalintent/CrimeLab.java
@@ -0,0 +1,46 @@
+package com.example.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by merz_konstantin on 5/13/17.
+ */
+
+public class CrimeLab { // Singleton-Klasse
+    public static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes; // Crime-"array"
+
+    public static CrimeLab get(Context context){ // stellt sicher, dass es maximal eine instanz von crimelab gibt
+        if(sCrimeLab==null){
+            sCrimeLab=new CrimeLab(context);
+        }
+        return  sCrimeLab;
+    }
+
+    private CrimeLab(Context context){ // privater konstruktor
+        mCrimes=new ArrayList<>();
+        // fuer starter erstellen wir selber neue crimes
+        for(int i=0;i<100;i++){
+            Crime crime=new Crime();
+            crime.setTitle("Crime #" +i);
+            crime.setSolved(i%2==0); // jede zweite untat ist geloest
+            mCrimes.add(crime);
+        }
+    }
+
+    public List<Crime> getCrimes(){ return mCrimes;} // gibt Crime-Array zurueck
+
+    public Crime getCrime(UUID id){ // gibt gesuchtes crime anhand der id zurueck
+        for (Crime crime:mCrimes){
+            if(crime.getId().equals(id)){
+                return crime;
+            }
+        }
+        return null;
+    }
+
+}
