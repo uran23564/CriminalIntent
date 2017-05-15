967a07b43c0ab8af45c5f455da314f561f58f3c0
uran23564 <uran23564@gmail.com>
2017-05-15 14:48:58 +0200

    asd

--- app/src/main/java/com/example/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/example/criminalintent/CrimePagerActivity.java
@@ -4,12 +4,16 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 
 public class CrimePagerActivity extends AppCompatActivity{
     private static final String EXTRA_CRIME_ID="com.example.criminalintent.crime_id";
 
     private ViewPager mViewPager;
+    private Button mFirstCrime;
+    private Button mLastCrime;
+    
     private List<Crime> mCrimes;
     
     public static Intent newIntent(Context packageContext,UUID crimeId){
@@ -25,7 +29,10 @@ public class CrimePagerActivity extends AppCompatActivity{
         
         UUID crimeId=(UUID) getIntent.getSerializable(EXTRA_CRIME_ID);
         
-        mViewPager=(ViewPager) findViewById(R.id.crime_view_pager); // initialisiere Widgets (hier nur den ViewPager)
+        // initialisiere Widgets
+        mViewPager=(ViewPager) findViewById(R.id.crime_view_pager); 
+        mFirstCrime=(Button) findViewById(R.id.first_crime_button);
+        mLastCrime=(Button) findViewById(R.id.last_crime_button);
         
         mCrimes=CrimeLab.get(this).getCrimes(); // rufe liste der crimes ab
         
@@ -36,6 +43,8 @@ public class CrimePagerActivity extends AppCompatActivity{
             @Override
             public Fragment getItem(int position){
                 Crime crime=mCrimes.get(position);
+                mFirstButton.setEnabled(position==0 ? false:true); // knopf deaktivieren, wenn wir an erster stelle sind
+                mLastButton.setEnabled(position==mCrimes.size()-1 false:true); // knopf deaktivieren, wenn wir an letzter stelle sind
                 return CrimeFragment.newInstance(crime.getId());
             }
             
@@ -51,5 +60,21 @@ public class CrimePagerActivity extends AppCompatActivity{
                 break;
             }
         }
+        
+
+        
+        mFirstButton.setOnClickListener(new View.OnClickListener(){ // zur ersten Untat springen
+            @Override
+            public void onClick(View v){
+                mViewPager.setCurrentItem(0);
+            }
+        });
+        
+        mLastButton.setOnClickListener(new View.OnClickListener(){ // zur ersten Untat springen
+            @Override
+            public void onClick(View v){
+                mViewPager.setCurrentItem(mCrimes.size()-1);
+            }
+        });
     }
 }
